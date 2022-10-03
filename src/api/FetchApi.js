9f48605f298d2c9f8c73bcc20923e345e2e680c0
golam mostafa <gmnayeem25@gmail.com>
2022-10-03 22:45:59 +0600

    create api directory

---
+++ src/api/FetchApi.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const FetchApi = (props) => {
+    const {setUsers, setIsLoading, setErrors} = props
+
+    const getAllUsers = () => {
+        fetch(URL)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Data not found")
+                }
+                return res.json()
+            })
+            .then(data => {
+                setUsers(data.users)
+            })
+            .catch(error => {
+                setErrors(error)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }
+
+    return (
+        <>
+            <getAllUsers />
+        </>
+    )
+  
+}
+
+export default FetchApi
+
+
